e99f2af71c48c841bbe9bed90fda0613b7241679
Mike Demick <mikedemick@yahoo.com>
2016-07-02 15:23:59 -0400

    inital commit

---
+++ SQL/sp_getUserBankBalanceCalculated.sql
@@ -0,0 +1,33 @@
+USE `PlayGFC7`;
+DROP procedure IF EXISTS `sp_getUserBankBalanceCalculated`;
+
+DELIMITER $$
+USE `PlayGFC7`$$
+CREATE DEFINER=`gfc`@`localhost` PROCEDURE `sp_getUserBankBalanceCalculated`(IN uid integer)
+BEGIN
+
+SELECT
+	SUM(val.field_transaction_value_value) as balance
+FROM
+	PlayGFC7.node bl
+JOIN
+	PlayGFC7.field_data_field_transaction_value val
+ON
+	bl.vid = val.revision_id
+JOIN
+	PlayGFC7.field_data_field_transaction_user usr
+ON
+	bl.vid = usr.revision_id
+JOIN
+	PlayGFC7.field_data_field_transaction_type trtype
+ON
+	bl.vid = trtype.revision_id
+WHERE
+	bl.type = 'user_bank_ledger'
+AND
+    usr.field_transaction_user_target_id = uid;
+
+END$$
+
+DELIMITER ;
+
