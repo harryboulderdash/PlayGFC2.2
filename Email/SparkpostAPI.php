78b00bcebcbe62f09c6d45f015cda9a8f200215e
Mike Demick <mikedemick@yahoo.com>
2016-07-18 21:31:41 -0400

    inital commit

---
+++ Email/SparkpostAPI.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikedemick
+ * Date: 7/18/16
+ * Time: 8:21 PM
+ */
+
+//template ID:                  gfc-kickoff-v1-7-18-2016
+//list ID (use as recipient):   gfc-users-july-18-2016
+
+
+//list_id	string	Identifier of the stored recipient list to use	yes	Specify this field when using a stored recipient list.
+
+
